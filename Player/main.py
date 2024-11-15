09543a15526bc0668ac6e8c6d00c57138adb7696
alxm9 <alxmoc96@gmail.com>
2024-11-15 17:18:37 +0000

    fixed controls

--- Player/main.py
+++ Player/main.py
@@ -11,29 +11,32 @@ id = "test"
 
 """
 For some reason str(details[0],'utf8') returns everything with "???" attached at the beginning. ( "???whatever" )
-[3:] is merely a workaround. Need to figure out why it's doing that.
+[3:] is merely a workaround.. Need to figure out why it's doing that.
 """
 
 def snake_loop():
     counter = 0
-    display.show(Image.HAPPY) # placeholder
+    display.show(Image("00000:06060:90009:06060:00000"))
+
     while True:
         details = radio.receive_full()
         if details:
-            if str(details[0],'utf8')[3:] == "stop": #WIP
-                speech.say("broken")
-                return
-        if counter == 1000: # breaks after a while if stop signal not detected
-            speech.say("broken")
-            return
-        if button_a.is_pressed():
+            if str(details[0],'utf8')[3:] == "stop":
+                break
+        if counter == 7500: # breaks after a while if stop signal not detected
+            break
+        if button_a.was_pressed():
             radio.send("a")
             counter = 0
-        elif button_b.is_pressed():
+        if button_b.was_pressed():
             radio.send("b")
             counter = 0
-        time.sleep(0.01)
+        time.sleep(0.001)
         counter += 1
+
+    speech.say("disconnected")
+    display.show(Image("00000:00000:00000:00000:00000"))
+    
 
 while True:
     details = radio.receive_full()
